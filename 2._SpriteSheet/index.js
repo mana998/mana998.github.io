bd03202e7d1d3e262ddb1ae7972b9ac84b3261d7
mana998 <mana998@azet.sk>
2021-02-24 22:39:01 +0100

    Added updates

--- 2._SpriteSheet/index.js
+++ 2._SpriteSheet/index.js
@@ -5,9 +5,9 @@ let lastTime;
 const requiredElapsed = 1000 / 30; //30 fps
 
 const distance = 10;
-const fontSize = canvas.width / 20
-const fontType = "Arial"
-const fontText = `${fontSize}px ${fontType}`
+let fontSize = canvas.width / 90;
+const fontType = "Arial";
+let fontText = `${fontSize}px ${fontType}`;
 
 let game;
 let isGameOver = false;
@@ -19,7 +19,7 @@ let animationFrame;
 //SPRITES
 //character sprite
 const bard = new Image();
-bard.src  = "./assets/bard.png";
+bard.src  = "./assets/images/character/bard.png";
 const spriteWidth = 32;
 const spriteHeight = 32;
 let bardDownImg = new Img(bard, 0, 1, 0, 1, 3, spriteWidth, spriteHeight);
@@ -28,18 +28,19 @@ let bardRightImg = new Img(bard, 2, 0, 0, 2, 3, spriteWidth, spriteHeight);
 let bardUpImg = new Img(bard, 3, 0, 0, 2, 3, spriteWidth, spriteHeight);
 //heart sprite
 const heart= new Image();
-heart.src  = "./assets/heart.png";
+heart.src  = "./assets/images/other/heart.png";
 const heartWidth = 16;
 const heartHeight = 16;
 let heartImg = new Img(heart, 0, 0, 0, 0, 10, heartWidth, heartHeight);
 //character
 let character = new Character(bardDownImg, spriteWidth, spriteHeight, canvas.width/2, canvas.height/2);
-
+//background
+let backgrounds;
 //sound
-const collectSound = new sound("./assets/sounds/collect.wav");
-const wrongSound = new sound("./assets/sounds/wrong.wav");
-const gameOverSound = new sound("./assets/sounds/game_over.wav");
-const selectSound = new sound("./assets/sounds/select.wav");
+const collectSound = new Sound("./assets/sounds/collect.wav");
+const wrongSound = new Sound("./assets/sounds/wrong.wav");
+const gameOverSound = new Sound("./assets/sounds/game_over.wav");
+const selectSound = new Sound("./assets/sounds/select.wav");
 
 //array of objects
 //let gameObjects = game.getCoins();
@@ -58,11 +59,11 @@ difficulties.set(hard, 8);
 
 window.addEventListener("load",
     () => {
+        backgrounds = new Background().getBackgrounds();
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        /*moneyImg.onload = () => {
-            setup();
-        }*/
+        fontSize = canvas.width / 90;
+        fontText = `${fontSize}px ${fontType}`;
         setup();
     }
 );
@@ -71,6 +72,8 @@ window.addEventListener("resize",
     () => {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
+        fontSize = canvas.width / 90;
+        fontText = `${fontSize}px ${fontType}`;
         setup();
     }
 );
@@ -92,21 +95,25 @@ function move(e){
     switch (e.key) {
         case "w":
         case "w":
+        case "ArrowUp":
             character.source = bardUpImg;
             character.y -= distance;
             break;
         case "s":
         case "S":
+        case "ArrowDown":
             character.source = bardDownImg;
             character.y += distance;
             break;
         case "d":
         case "D":
+        case "ArrowRight":
             character.source = bardRightImg;
             character.x += distance;
             break;
         case "a":
         case "A":
+        case "ArrowLeft":
             character.source = bardLeftImg;
             character.x -= distance;
             break;
@@ -166,19 +173,18 @@ function draw(now){
     if (elapsed > requiredElapsed) {
         drawBackground();
         update();
-        console
         if (isGameOver) {
             //need to set size
             writeText(canvas.width/2, canvas.height/2 - fontSize, "GAME OVER", "black", "center", `${fontSize*2}px ${fontType}`);
             writeText(canvas.width/2, canvas.height/2 + fontSize, "Press R to Restart", "black");
         } else {
-            detectCollisions();
+            game.detectCollisions();
             drawBackground();
             for (const gameObject of gameObjects) {
                 if (gameObject.isColliding && !gameObject.source){
                     if(gameObjects[0].src === gameObject.src){
                         if (gameObjects[0] !== gameObject) {
-                            switchPosition(gameObjects[0], gameObject);
+                            game.switchPosition(gameObjects[0], gameObject);
                         }
                         gameObjects.shift();
                         collectSound.play();
@@ -216,68 +222,29 @@ function writeText(x, y, text, color, align, font) {
     ctx.fillText(text, x, y);
 }
 
-function switchPosition(obj1, obj2) {
-    let index1 = gameObjects.indexOf(obj1);
-    let index2 = gameObjects.indexOf(obj2);
-    let tempObject = gameObjects[index1];
-    gameObjects[index1] = gameObjects[index2];
-    gameObjects[index2] = tempObject;
-}
-
 function drawBackground() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.log(game.subLevel);
+    console.log(backgrounds[1]);
+    let selected = game.level || 1;
+    let grassPattern = ctx.createPattern(backgrounds[selected - 1], "repeat"); 
+    ctx.fillStyle = grassPattern;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function drawImage(img){
     img.draw(ctx);
 }
 
-function detectCollisions(){
-    let obj1;
-    let obj2;
-
-    // Reset collision state of all objects
-    for (let i = 0; i < gameObjects.length; i++) {
-        gameObjects[i].isColliding = false;
-    }
-
-    // Start checking for collisions
-    for (let i = 0; i < gameObjects.length; i++) {
-        obj1 = gameObjects[i];
-
-        for (let j = i + 1; j < gameObjects.length; j++)
-        {
-            obj2 = gameObjects[j];
-
-            // Compare object1 with object2
-            if (rectIntersect(obj1.x, obj1.y, obj1.width, obj1.height, obj2.x, obj2.y, obj2.width, obj2.height)){
-                obj1.isColliding = true;
-                obj2.isColliding = true;
-            }
-        }
-    }
-}
-
-function rectIntersect(x1, y1, w1, h1, x2, y2, w2, h2) {
-    // Check x and y for overlap
-    if (x2 > w1 + x1 || x1 > w2 + x2 || y2 > h1 + y1 || y1 > h2 + y2){
-        return false;
-    }
-    return true;
-}
-
  function update(){
     if (game.hp > 0) {
         if (gameObjects.length === 1){
-            console.log(gameObjects.length);
-            console.log(game.amount);
             game.nextLvl();
             gameObjects = game.getCoins();
             showCoinsOrder();
             //show order and then continue
             setTimeout(function(){
                 character.resetPosition(canvas);
-                repositionCoins();
+                game.repositionCoins(canvas, character);
                 //character.resetPosition(canvas);
                 gameObjects.push(character);
             }, game.wait);
@@ -297,38 +264,3 @@ function showCoinsOrder(){
         x += game.coinWidth * 3/2;
     }
 }
-
-function repositionCoins(){
-    for (let i = 0; i < gameObjects.length; i++) {
-        let isColliding = false;
-        do {
-            gameObjects[i].x = Math.floor(Math.random()*canvas.width);
-            gameObjects[i].y = Math.floor(Math.random()*canvas.height);
-            //make sure coins are displayed fully on screen
-            if (gameObjects[i].x < game.coinWidth) gameObjects[i].x += game.coinWidth;
-            if (gameObjects[i].x > canvas.width - game.coinWidth) gameObjects[i].x -= game.coinWidth;
-            if (gameObjects[i].y < game.coinHeight) gameObjects[i].y += game.coinHeight;
-            if (gameObjects[i].y > canvas.height - game.coinHeight) gameObjects[i].y -= game.coinHeight;
-            for (let j = 0; j < i; j++) {
-                isColliding = rectIntersect(gameObjects[i].x, gameObjects[i].y, gameObjects[i].width, gameObjects[i].height, gameObjects[j].x, gameObjects[j].y, gameObjects[j].width, gameObjects[j].height);
-                if (isColliding) {break;}
-            }
-            isColliding = rectIntersect(gameObjects[i].x, gameObjects[i].y, gameObjects[i].width, gameObjects[i].height, character.x, character.y, character.width, character.height);
-        } while (isColliding);
-    }
-}
-
-function sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function(){
-      this.sound.play();
-    }
-    this.stop = function(){
-      this.sound.pause();
-    }
-  }
---
+++ 2._SpriteSheet/models/Background.js
@@ -0,0 +1,19 @@
+const green = new Image();
+green.src = "./assets/images/background/green.jpg";
+const orange = new Image();
+orange.src = "./assets/images/background/orange.jpg";
+const blue = new Image();
+blue.src = "./assets/images/background/blue.jpg";
+const red = new Image();
+red.src = "./assets/images/background/red.jpg";
+const gray = new Image();
+gray.src = "./assets/images/background/gray.jpg";
+
+class Background {
+    constructor() {
+        this.backgrounds = [green, orange, red, blue, gray];
+    }
+    getBackgrounds() {
+        return this.backgrounds;
+    }
+}
